8a71b665d78e2bc5218c270e9422278c88c6d4dd
CharlieNiz <nizcar1@gmail.com>
2023-03-14 04:30:24 -0300

    Update Script01.js

--- js/Script01.js
+++ js/Script01.js
@@ -1,8 +1,9 @@
-alert("Bienvenido al simulador de Plazos fijos y Prestamos") //Calculador de plazo fijo a 12 meses con tasa fija anual de 75% en pesos o 10% anual en USD
+//alert("Bienvenido al simulador de Plazos fijos y Prestamos") //Calculador de plazo fijo a 12 meses con tasa fija anual de 75% en pesos o 10% anual en USD
 let monto = 0; let interes = 0; let total = 0; let tasa = 0; let meses = 0; //declaración de variables
+let plazo = []; let ganancia = [];
 
 function PlazoFijo() { //función para cálculo de los plazos fijos
-    monto = prompt("Ingrese monto en " + moneda + " a invertir"); //solicitud de ingreso del valor de dinero a invertir
+    monto = prompt("Ingrese monto en a invertir"); //solicitud de ingreso del valor de dinero a invertir
     while (isNaN(monto) == true || monto == "") { //mientras 'monto' no sea un número o sea nulo continuará solicitando ingreso
         alert("Debe ingresar un valor numérico"); //advertencia de valor ingresado incorrecto
         monto = prompt("Ingrese monto a invertir"); //nueva solicitud de ingreso
@@ -14,31 +15,39 @@ function PlazoFijo() { //función para cálculo de los plazos fijos
     }
     interes = monto * tasa / 365 * meses * 30; //formula para cálculo del 'interes' anual aplicado a los días según la 'tasa' correspondiente
     total = parseInt(monto) + parseFloat(interes); //guardado de datos númericos para calcular el total a reembolsar
-    alert("Tasa anual : " + tasa * 100 + "% \nPlazo: " + meses * 30 + " dias\nIntereses: $ " //muestra de los resultados obtenidos
-        + interes.toFixed(2) + " " + moneda + "\nTotal a reembolsar: $" + total.toFixed(2) + " " + moneda);
-    const PlazoFijoSafe = []; //creación de array para guardado de datos
-    PlazoFijoSafe.push((monto), (tasa), (meses), (interes.toFixed(2)), total.toFixed(2),); //guardado de datos para ordenar posteriormente
-    console.log(PlazoFijoSafe);
+    alert("Tasa anual : " + tasa * 100 + "% \nPlazo: " + meses * 30 + " dias\nIntereses: $ " + interes.toFixed(2) + "\nTotal a reembolsar: $" + total.toFixed(2));
+
+    plazo.push(meses); console.log(meses);
+    ganancia.push(total.toFixed(2)); console.log(ganancia);
 }
 
-function Ordenar() { //función para ordenamiento de datos
-    alert("funcionalidad de ordenamiento en contrucción");
+function Orden() {
+    let opcion = prompt("Seleccione criterio de ordenamiento \n\n\n1 - Mayores ganancias generadas\n\n2 - Plazo más corto calculado\n\nENTER volver al menu anterior\n"); //menu principal
+    while (opcion != "") {
+        switch (opcion) { //dependiendo de la selección se establece el tipo de moneda y tasa correspondiente
+            case '1':
+                alert(ganancia.sort((a, b) => b - a));
+                break;
+            case '2':
+                alert(plazo.sort((a, b) => a - b));
+                break;
+            case '3':
+                break;
+        }
+        opcion = prompt("Seleccione nuevo criterio de ordenamiento \n\n\n1 - Mayores ganancias generadas\n\n2 - Plazo más corto calculado\n\nENTER para volver al menu anterior\n");
+    }
 }
 
-let menu = prompt("Seleccione la opción deseada \n\nOpción 1 Plazo fijo en Pesos ARS\nOpción 2 Plazo fijos en Dolares USD\nEnter para salir"); //menu principal
+let menu = prompt("Menu principal\n\n\nPresione 1 para calcular su primer plazo fijo en Pesos ARS\n\nENTER para SALIR\n"); //menu principal
 while (menu != "") {
     switch (menu) { //dependiendo de la selección se establece el tipo de moneda y tasa correspondiente
         case '1':
-            tasa = '.75'; moneda = "pesos" //moneda "pesos" tasa 75%
+            tasa = .75;
             PlazoFijo();
             break;
         case '2':
-            tasa = '.1'; moneda = "dolares" //moneda "dolares" tasa 10%
-            PlazoFijo();
-            break;
-        case '3':
-            Ordenar();
+            Orden();
             break;
     }
-    menu = prompt("¿Desea realizar otra operación? \n\nOpción 1 Plazo fijo en Pesos ARS\nOpción 2 Plazo fijos en Dolares USD\nOpción 3 Ordenar consultas existentes\nEnter para salir");
+    menu = prompt("¿Desea realizar otra operación? \n\n\n1 - Calcular nuevo plazo fijo en Pesos ARS\n\n2 - Ordenar consultas existentes\n\nENTER para SALIR\n");
 }
