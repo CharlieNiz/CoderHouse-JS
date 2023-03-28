0fe2d3982a4ec4ea4b8fa653b4431273a2e514ae
CharlieNiz <nizcar1@gmail.com>
2023-03-28 19:12:22 -0300

    Update Script01.js

--- js/Script01.js
+++ js/Script01.js
@@ -1,5 +1,4 @@
 let operaciones = [];
-
 const //Elementos del DOM
     inputInversion = document.getElementById('inputInvest'),
     selectDuracion = document.getElementById('selectDuration'),
@@ -8,29 +7,29 @@ const //Elementos del DOM
     show = document.getElementById('text');
 
 class Plazo { //Método constructor
-    constructor(inversion, duracion, reembolsar, id) {
+    constructor(inversion, duracion, reembolsar) {
         this.inversion = parseInt(inversion);
         this.duracion = parseInt(duracion);
         this.reembolsar = parseFloat(reembolsar);
     }
 }
 
-function Valores (ingresos){   //tomo los valores cargados en el formulario
-    const calculos = new Plazo (inputInversion.value, selectDuracion.value, mostrarResultado.value)
+function Valores(ingresos) {   //tomo los valores cargados en el formulario
+    const calculos = new Plazo(inputInversion.value, selectDuracion.value, mostrarResultado.value)
     ingresos.push(calculos)
 }
 
-function GuardarValores(ingresos){
+function GuardarValores(ingresos) {
     localStorage.setItem('operacionesGuardadas', JSON.stringify(ingresos))
 }
 
 function PlazoFijo() { //función para cálculo de los plazos fijos
     let inversion = inputInversion.value;
     let duracion = selectDuracion.value;
-    let interes = inversion  * .75 / 365 * duracion * 30; //formula para cálculo del 'interes' anual aplicado a los días según la 'tasa' correspondiente
+    let interes = inversion * .75 / 365 * duracion * 30; //formula para cálculo del 'interes' anual aplicado a los días según la 'tasa' correspondiente
     let total = parseInt(inversion) + parseFloat(interes); //guardado de datos numericos para calcular el total a reembolsar
     reembolsar = total.toFixed(2);
-    show.innerHTML = "<br>Con su inversión de:"+"<b> $"+inversion+"</b>"+"<br>Durante un periodo de: <b>"+duracion*30+"</b> días"+"<br>Otendrá: $<b>"+reembolsar+"</b> pesos";
+    show.innerHTML = "<br>Con su inversión de:" + "<b> $" + inversion + "</b>" + "<br>Durante un periodo de: <b>" + duracion * 30 + "</b> días" + "<br>Otendrá: $<b>" + reembolsar + "</b> pesos";
     mostrarResultado.value = `${reembolsar}`;
     Valores(operaciones);
     GuardarValores(operaciones);
